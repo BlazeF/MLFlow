c9dfde2b0cad1b7d1703aa4a1ab70109b7d61d6c
BlazeF <87708770+BlazeF@users.noreply.github.com>
2023-06-23 18:18:14 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,34 @@
+#print("hello world")
+"""
+if 5 > 2 : 
+    print("Five is greater than two")
+
+
+x = 5 
+x = "tera naam"
+print(x)
+
+"""
+y = int(3)
+z = str("rahul")
+m = float(5)
+
+a = 6 
+v = "float"
+
+print(type(a)) 
+
+print(type(v))
+
+# x , y , z = "Orange" , "Cherry" , "Papaya"
+
+# fruits =["Anaar" , "Aam" , "Tarbooj"]
+# x , y , z = fruits
+# print(x , y , z)
+# is same as 
+# print (x + y + z )
+
+
+# p =5 
+# q = 10 
+# print(p+q)                "must be same data type"
